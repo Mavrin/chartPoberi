98cc86ebde50d4c887e6dee65dd1bc57a5154270
konstantin <krivlenia@targetprocess.com>
2014-10-18 12:44:36 +0300

    fix publish task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
 	require('load-grunt-tasks')(grunt);
 
@@ -25,7 +25,19 @@ module.exports = function(grunt) {
 						'!package.json'
 					],
 					dest: 'temp/pres/'
-				},{
+				}, {
+					expand: true,
+					cwd: 'bower_components/tauCharts/',
+					src: [
+						'build/tauCharts.min.js',
+						'src/tau.plugins.js',
+						'src/class.js',
+						'plugins/tooltip.js',
+						'plugins/highlighter.js',
+						'css/**'
+					],
+					dest: 'temp/pres/tauCharts/'
+				}, {
 					expand: true,
 					cwd: 'node_modules/shower-core/',
 					src: [
@@ -34,7 +46,36 @@ module.exports = function(grunt) {
 						'!Readme.md'
 					],
 					dest: 'temp/pres/shower/'
-				},{
+				}, {
+					expand: true,
+					cwd: 'bower_components/d3/',
+					src: [
+						'd3.min.js',
+					],
+					dest: 'temp/pres/d3/'
+				}, {
+					expand: true,
+					cwd: 'bower_components/highcharts/',
+					src: [
+						'highcharts.js',
+					],
+					dest: 'temp/pres/highcharts/'
+				}, {
+					expand: true,
+					cwd: 'bower_components/underscore/',
+					src: [
+						'underscore.js',
+					],
+					dest: 'temp/pres/underscore/'
+				}, {
+					expand: true,
+					cwd: 'bower_components/jquery/',
+					src: [
+						'dist/jquery.min.js',
+						'dist/jquery.min.map'
+					],
+					dest: 'temp/pres/jquery/'
+				}, {
 					expand: true,
 					cwd: 'node_modules/shower-ribbon/',
 					src: [
@@ -43,7 +84,7 @@ module.exports = function(grunt) {
 						'!Readme.md'
 					],
 					dest: 'temp/pres/shower/themes/ribbon/'
-				},{
+				}, {
 					expand: true,
 					cwd: 'node_modules/shower-bright/',
 					src: [
@@ -62,9 +103,24 @@ module.exports = function(grunt) {
 				replacements: [{
 					from: /(node_modules|bower_components)\/shower-core/g,
 					to: 'shower'
-				},{
+				}, {
 					from: /(node_modules|bower_components)\/shower-(ribbon|bright)/g,
 					to: 'shower/themes/$2'
+				}, {
+					from: /(bower_components)\/jquery/g,
+					to: 'jquery'
+				}, {
+					from: /(bower_components)\/underscore/g,
+					to: 'underscore'
+				}, {
+					from: /(bower_components)\/tauCharts/g,
+					to: 'tauCharts'
+				}, {
+					from: /(bower_components)\/d3/g,
+					to: 'd3'
+				}, {
+					from: /(bower_components)\/highcharts/g,
+					to: 'highcharts'
 				}]
 			},
 			themes: {
